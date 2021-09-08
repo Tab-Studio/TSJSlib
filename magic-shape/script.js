26e1193da27c1a9c0a7cf4e660386f6819b7268f
鈦匕工作室｜Tab Studio <89435281+Tab-Studio@users.noreply.github.com>
2021-09-08 21:31:51 +0800

    Add files via upload

---
+++ magic-shape/script.js
@@ -0,0 +1,39 @@
+// include <Tab-Studio/TSJSlib/basic.js>
+// include <Tab-Studio/TSJSlib/color.js>
+// include <Tab-Studio/TSJSlib/magic-shape.js>
+
+// https://tab-studio.github.io/TSJSlib/magic-shape/script.js
+const cvs = $('#cvs'), 
+ctx = cvs.getContext('2d'), 
+msc = [color.brite.red, color.brite.green], 
+// msc = [color.brite.white, color.brite.gray], 
+ms = [];
+cvs.width = 100*vw();
+cvs.height = 100*vh();
+ctx.fillStyle = '#000000';
+ctx.fillRect(0, 0, cvs.width, cvs.height);
+function windowCWH(w = 0, h = 0){
+    if(w !== 100*vw() || h !== 100*vh()){
+        w = 100*vw();
+        h = 100*vh();
+        cvs.width = w;
+        cvs.height = h;
+    }
+    setTimeout((ww = w, hh = h) => {
+        windowCWH(ww, hh);
+    }, 30);
+}
+windowCWH();
+msc.forEach(c => ms.push(new magicShape({'c':c, 's':0.5, 'f':false})));
+function aniLoop(){
+    ctx.fillStyle = '#00000034';
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+    setTimeout(aniLoop, 30);
+    ms.forEach(msi => {
+        // let c = msi.c.replace('#', '');
+        // let cp = 0;
+        // msi.c = '#' + (parseInt(c[0] + c[1], 16)+cp).toString(16) + (parseInt(c[2] + c[3], 16)+cp).toString(16) + (parseInt(c[4] + c[5], 16)+cp).toString(16);
+        msi.update();
+    });
+}
+aniLoop();
