15fd2c658440f6bdf01acfd153995517d3d31102
鈦匕工作室｜Tab Studio <89435281+Tab-Studio@users.noreply.github.com>
2021-09-13 22:12:17 +0800

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,6 +1,10 @@
 // src https://tab-studio.github.io/TSJSlib/basic.js
-// version 2.0.0
+// version 2.0.2
 function $(e, f = document){return(f.querySelector(e));}
+function $$(e, f = document){return(f.querySelectorAll(e));}
+function vw(){return(window.innerWidth/100);}
+function vh(){return(window.innerHeight/100);}
+function random(min, max){return(Math.floor(Math.random()*(max+1-min))+min);}
 const keys = {}, 
 $_GET = {}, 
 $_COOKIE = {};
@@ -16,10 +20,54 @@ if(document.cookie !== ''){
         $_COOKIE[kv[0]] = kv[1];
     });
 }
-function $$(e, f = document){return(f.querySelectorAll(e));}
-function vw(){return(window.innerWidth/100);}
-function vh(){return(window.innerHeight/100);}
-function random(min, max){return(Math.floor(Math.random()*(max+1-min))+min);}
+function getGet(key = false){
+    let get = {};
+    if(location.href.indexOf('?') > -1){
+        location.href.split('?')[1].split('&').forEach(kv => {
+            kv = kv.split('=');
+            get[kv[0]] = kv[1];
+        });
+    }
+    if(key !== false){
+        return(get[key]);
+    }
+    else{
+        return(get);
+    }
+}
+function getCookie(key = false){
+    let cookie = {};
+    if(document.cookie !== ''){
+        document.cookie.split('; ').forEach(kv => {
+            kv = kv.split('=');
+            cookie[kv[0]] = kv[1];
+        });
+    }
+    if(key !== false){
+        return(cookie[key]);
+    }
+    else{
+        return(cookie);
+    }
+}
+function setCookie(key = undefined, value = undefined, expire = undefined, path = undefined, domain = undefined, secure = undefined){
+    let cookie = '';
+    if(key !== undefined && value !== undefined){
+        cookie = `${key}=${value}`;
+        if(expire !== undefined){
+            cookie += `; expires=${expire}`;
+        }
+        if(path !== undefined){
+            cookie += `; path=${path}`;
+        }
+        if(domain !== undefined){
+            cookie += `; domain=${domain}`;
+        }
+        if(secure !== undefined){
+            cookie += `; secure`;
+        }
+    }
+}
 function sendXmlhttp(name = '', value = '', responseFunction = t => {console.log(t);}, type = 'get'){
     let xmlhttp = new XMLHttpRequest();
     let rf = function (){
@@ -49,4 +97,10 @@ function webLoad(link){
 }
 function webOpen(link){
     window.open(link);
+}
+function radians(deg){
+    return(deg * (Math.PI / 180));
+}
+function deg(radians){
+    return(radians / (Math.PI / 180));
 }
