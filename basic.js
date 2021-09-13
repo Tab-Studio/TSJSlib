a32b42bc69cf83206b831c885d56b14d8b034839
鈦匕工作室｜Tab Studio <89435281+Tab-Studio@users.noreply.github.com>
2021-09-13 21:40:58 +0800

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,6 +1,52 @@
-// https://tab-studio.github.io/TSJSlib/basic.js
+// src https://tab-studio.github.io/TSJSlib/basic.js
+// version 2.0.0
 function $(e, f = document){return(f.querySelector(e));}
+const keys = {}, 
+$_GET = {}, 
+$_COOKIE = {};
+if(location.href.indexOf('?') > -1){
+    location.href.split('?')[1].split('&').forEach(kv => {
+        kv = kv.split('=');
+        $_GET[kv[0]] = kv[1];
+    });
+}
+if(document.cookie !== ''){
+    document.cookie.split('; ').forEach(kv => {
+        kv = kv.split('=');
+        $_COOKIE[kv[0]] = kv[1];
+    });
+}
 function $$(e, f = document){return(f.querySelectorAll(e));}
 function vw(){return(window.innerWidth/100);}
 function vh(){return(window.innerHeight/100);}
 function random(min, max){return(Math.floor(Math.random()*(max+1-min))+min);}
+function sendXmlhttp(name = '', value = '', responseFunction = t => {console.log(t);}, type = 'get'){
+    let xmlhttp = new XMLHttpRequest();
+    let rf = function (){
+        if (xmlhttp.readyState==4) {
+            responseFunction(xmlhttp.responseText);
+        }
+    }
+    type = type.toLowerCase();
+    xmlhttp.addEventListener("readystatechange", rf);
+    if(type == 'get'){
+        xmlhttp.open("GET", name+value);
+        xmlhttp.send();
+    }
+    else if(type == 'post'){
+        xmlhttp.open("POST", name,true);
+        xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xmlhttp.send(value);
+    }
+}
+function webLoad(link){
+    if(keys['Control']){
+        window.open(link);
+    }
+    else{
+        location.href = link;
+    }
+}
+function webOpen(link){
+    window.open(link);
+}
